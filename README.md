07dfc2640517b46c18e8692672a476802de1ea47
RuiRomano <ruiromano@microsoft.com>
2025-05-16 11:30:05 +0100

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-# pbip-demo
---
+++ deploy.py
@@ -0,0 +1,38 @@
+# Install fabric-cicd: https://microsoft.github.io/fabric-cicd/
+from azure.identity import InteractiveBrowserCredential, ClientSecretCredential
+from fabric_cicd import FabricWorkspace, publish_all_items, change_log_level
+import argparse
+import os
+
+#change_log_level("DEBUG")
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--spn-auth", default = False)
+parser.add_argument("--workspace", default = "RR - PBIR - SLL")
+
+args = parser.parse_args()
+
+spn_auth = args.spn_auth
+workspace_name = args.workspace
+
+# Authentication
+
+if (not spn_auth):
+    credential = InteractiveBrowserCredential() 
+else:
+    client_id = os.getenv("FABRIC_CLIENT_ID")
+    client_secret = os.getenv("FABRIC_CLIENT_SECRET")
+    tenant_id = os.getenv("FABRIC_TENANT_ID")
+
+    credential = ClientSecretCredential(client_id=client_id, client_secret=client_secret, tenant_id=tenant_id)
+
+workspace_name = "RR - PBIR - SLL"
+
+target_workspace = FabricWorkspace(    
+    workspace_name = workspace_name,    
+    repository_directory = ".\\",
+    item_type_in_scope = ["SemanticModel", "Report"],        
+    token_credential = credential,
+)
+
+publish_all_items(target_workspace)
