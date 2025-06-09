53d6f13bb0baab9c2361250bb27bddddfd4a5c1a
Mucahit Katirci <mucahit_katirci@epam.com>
2025-06-09 11:37:45 +0300

    secret and client id

--- deploy.py
+++ deploy.py
@@ -17,16 +17,16 @@ args = parser.parse_args()
 spn_auth = args.spn_auth
 workspace_name = args.workspace
 src_path = args.src
-environment = args.environment
+environment = args.environment+
 
 # Authentication (SPN or Interactive)
 
 if (not spn_auth):
     credential = InteractiveBrowserCredential() 
 else:
-    client_id = os.getenv("FABRIC_CLIENT_ID")
-    client_secret = os.getenv("FABRIC_CLIENT_SECRET")
-    tenant_id = os.getenv("FABRIC_TENANT_ID")
+    client_id = os.getenv("905324ca-ce7f-4f86-9a71-95b6487d52bd")
+    client_secret = os.getenv("8cac0080-384b-4cca-864b-a10030e75d43")
+    tenant_id = os.getenv("72e63bb5-ee93-4204-beed-cdcb89e22dcf")
 
     credential = ClientSecretCredential(client_id=client_id, client_secret=client_secret, tenant_id=tenant_id)
 
